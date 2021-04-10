d306a6daca489b70783e8147077e849ea33110af
Seyed Younes Mirmohammadi <mir.younes01@gmail.com>
2021-04-10 20:54:27 +0430

    profile model created

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+
+    GENDER = (
+        ('male','Male'),
+        ('female','Female'),
+        ('other','Other')
+    )
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE , related_name="profile")
+    age = models.PositiveSmallIntegerField(null=True , blank=True)
+    gender = models.CharField(max_length=100 , choices=GENDER , null=True , blank=True)
+    phone_number = models.BigIntegerField(null=True , blank=True)
+    bio = models.TextField(null=True , blank=True)
+    avtar = models.ImageField(upload_to='avatars', default='avatars/cover.png' , null=True , blank=True)
