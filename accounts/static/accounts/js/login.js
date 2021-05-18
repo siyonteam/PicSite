b96b1bd1613c89dd87c07ccf04f16e509145a334
Siamak Alipour <siamakalipour37@gmail.com>
2021-05-18 16:22:59 +0430

    login page added for test

---
+++ accounts/static/accounts/js/login.js
@@ -0,0 +1,20 @@
+let regBox = document.querySelector(".registerBox");
+let logBox = document.querySelector(".loginBox");
+const classClean = () => {
+    logBox.classList.remove("enter");
+    logBox.classList.remove("exit");
+    regBox.classList.remove("enter");
+    regBox.classList.remove("exit");
+};
+logBox.addEventListener("click", (e) => {
+    e.preventDefault();
+    classClean();
+    logBox.classList.add("enter");
+    regBox.classList.add("exit");
+});
+regBox.addEventListener("click", (e) => {
+    e.preventDefault();
+    classClean();
+    regBox.classList.add("enter");
+    logBox.classList.add("exit");
+});
