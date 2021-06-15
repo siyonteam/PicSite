ee689f095cafad0c768f22dbf4ccaedff05690dd
Seyed Younes Mirmohammadi <mir.younes01@gmail.com>
2021-06-15 12:09:22 +0430

    fixed pagelink bug

--- accounts/static/accounts/js/pageLink.js
+++ accounts/static/accounts/js/pageLink.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
-    $(".page-link").click(function (e) {
+    $("body").on("click" , ".page-link",function (e) {
         e.preventDefault();
-        alert("hi");
         let page = $(this).attr("data-page");
         $.get("?page=" + page, function (data) {
             document.getElementById("profile_pics").innerHTML = data;
